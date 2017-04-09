cb1fa55a0f0dc4e2d958d81e539088e2015bcf84
Jake Boyles <jake@jibdesigns.com>
2017-04-09 13:32:24 -0400

    export styles

--- index.ios.js
+++ index.ios.js
@@ -7,6 +7,7 @@ import {
   TextInput
 } from 'react-native';
 
+import cssStyles from './styles';
 import axios from 'axios';
 
 export default class TrumpSearch extends Component {
@@ -31,10 +32,10 @@ export default class TrumpSearch extends Component {
     });
   }
 
-  changeQuote(text){
+  changeQuote(data){
     let _this = this;
     axios.post('https://trumpdonald.herokuapp.com/search',{
-      search:text.text
+      search:data.text
     })
     .then(response => {
       let rando = Math.floor(Math.random() * response.data._embedded.quotes.length) + 1;
@@ -69,46 +70,6 @@ export default class TrumpSearch extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#ffca92',
-  },
-  input:{
-    height: 40,
-    backgroundColor:'#fff',
-    color:'black',
-    marginTop:50,
-    marginBottom:30,
-    width:'90%',
-    marginLeft:'5%',
-    padding:10
-  },
-  welcome: {
-    fontSize: 30,
-    textAlign: 'center',
-    margin: 10,
-    color:'#fff',
-    fontWeight:'800',
-    paddingLeft:40,
-    paddingRight:40,
-    textShadowColor:'rgba(0,0,0,.2)',
-    textShadowOffset:{width:1,height:1},
-    textShadowRadius:2
-  },
-  quotes: {
-    textAlign: 'center',
-    color: '#fff',
-    marginTop: 15,
-    padding:20,
-    lineHeight:25,
-    fontSize:22,
-    textShadowColor:'rgba(0,0,0,.2)',
-    textShadowOffset:{width:1,height:1},
-    textShadowRadius:2
-  },
-});
+const styles = StyleSheet.create(cssStyles);
 
 AppRegistry.registerComponent('TrumpSearch', () => TrumpSearch);
---
+++ styles.js
@@ -0,0 +1,41 @@
+export default {
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#ffca92',
+  },
+  input:{
+    height: 40,
+    backgroundColor:'#fff',
+    color:'black',
+    marginTop:50,
+    marginBottom:30,
+    width:'90%',
+    marginLeft:'5%',
+    padding:10
+  },
+  welcome: {
+    fontSize: 30,
+    textAlign: 'center',
+    margin: 10,
+    color:'#fff',
+    fontWeight:'800',
+    paddingLeft:40,
+    paddingRight:40,
+    textShadowColor:'rgba(0,0,0,.2)',
+    textShadowOffset:{width:1,height:1},
+    textShadowRadius:2
+  },
+  quotes: {
+    textAlign: 'center',
+    color: '#fff',
+    marginTop: 15,
+    padding:20,
+    lineHeight:25,
+    fontSize:22,
+    textShadowColor:'rgba(0,0,0,.2)',
+    textShadowOffset:{width:1,height:1},
+    textShadowRadius:2
+  },
+}
